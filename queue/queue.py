fabeadda1c10b0bb6595e6ee28d4f67c8f3d53f1
Robert Keffury <bobby.keffury@me.com>
2020-07-30 20:22:08 -0700

    Finished binary search tree & doubly linked list.

--- queue/queue.py
+++ queue/queue.py
@@ -19,15 +19,18 @@ from singly_linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
     
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def enqueue(self, value):
-        self.storage.append(value)
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if len(self.storage) == 0:
             return None
-        return self.storage.pop(0)
+        else:
+            self.size -= 1
+            return self.storage.remove_head()
